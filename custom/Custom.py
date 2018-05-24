45e35f75984bdec347c22a8547dd682c60f1827b
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-05-24 09:08:23 +0900

    Added Custom agent v8

--- custom/Custom.py
+++ custom/Custom.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 
-from anyrl.rollouts import PrioritizedReplayBuffer
+from anyrl.rollouts import PrioritizedReplayBuffer, UniformReplayBuffer
 
 
 class BufferAveragePRB(PrioritizedReplayBuffer):
@@ -212,4 +212,45 @@ class StochasticMaxPRB(PrioritizedReplayBuffer):
 
         while len(self.transitions) > self.capacity:
             # TODO Any way to update max here?
+            del self.transitions[0]
+
+
+class AllAverageURB(PrioritizedReplayBuffer):
+    """
+    A uniform replay buffer with All Average Threshold caching.
+    """
+    def __init__(self, capacity, alpha, beta, first_max=1, epsilon=0):
+        super().__init__(capacity, alpha, beta, first_max, epsilon)
+
+        self.error_threshold = 0 # Threshold error for newly added sample
+
+    def sample(self, num_samples):
+        res = [random.choice(self.transitions).copy() for _ in range(num_samples)]
+        for transition in res:
+            transition['weight'] = 1
+        return res
+
+    def add_sample(self, sample, init_weight=None):
+        """
+        Add a sample to the buffer.
+        When new samples are added without an explicit initial weight, the
+        maximum weight argument ever seen is used. When the buffer is empty,
+        first_max is used.
+        """
+        if init_weight is None:
+            new_error = self._process_weight(self._max_weight_arg)
+
+            self.transitions.append(sample)
+            self.errors.append(new_error)
+        else:
+            new_error = self._process_weight(init_weight)
+
+            if new_error >= self.error_threshold:
+                self.transitions.append(sample)
+                self.errors.append(new_error)
+
+        # Update error_threshold via moving average
+        self.error_threshold += (new_error - self.error_threshold) / len(self.transitions)
+
+        while len(self.transitions) > self.capacity:
             del self.transitions[0]
